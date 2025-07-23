e7ae8d95164dc790c9d48aeff96c9ba374e534bc
cantabile1129 <kanta.fukao@keio.jp>
2025-07-23 15:29:39 +0900

    マルチチャンネルで記録，異なる音声ファイルを別々のスピーカーから流す．

--- audiofiles_switching/simpleaudio.py
+++
@@ -1,5 +0,0 @@
-import audiofiles_switching.simpleaudio as simpleaudio
-
-wav_obj = simpleaudio.WaveObject.from_wave_file("/home/takamichi-lab-pc05/ドキュメント/B4/concone_synthesizerV/concone_synthesizerV_sakiAI_trial1_MixDown.wav")
-play_obj = wav_obj.play()
-play_obj.wait_done()
